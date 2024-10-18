678c9832bf7a55ceecf797918a52d22c1cbf4902
kietcaodev <71913797+kietcaodev@users.noreply.github.com>
2024-10-18 08:30:09 +0700

    Update freeswitch_build.sh

--- freeswitch_build.sh
+++ freeswitch_build.sh
@@ -1,4 +1,25 @@
+echo "*	| __ )  __ _ ___  ___| __ ) ___|  |  _ \| __ ) \/ /	*"
+echo "*	|  _ \ / _,  / __|/ _ \  _ \___ \  | |_) |  _ \\  /	*"
+echo "*	| |_) | (_| \__ \  __/ |_) |__) | |  __/| |_) /  \	*"	 
+echo "*	|____/ \__,_|___/\___|____/____/  |_|   |____/_/\_\	*"	 
+echo "*****************************************************************"
+echo
+echo
+echo "Starting install Freeswitch version 1.10.11 !"
 yum update -y
+echo '[irontec]
+name=Irontec RPMs repository
+baseurl=http://packages.irontec.com/centos/$releasever/$basearch/' > /etc/yum.repos.d/irontec.repo
+rpm --import http://packages.irontec.com/public.key
+yum install sngrep -y
+yum install ntp -y 
+yum install -y chrony 
+systemctl restart chronyd 
+systemctl restart ntpd 
+systemctl enable ntpd
+systemctl enable chronyd 
+ntpdate -q vn.pool.ntp.org 
+timedatectl
 yum groupinstall -y 'Development Tools'
 yum install -y epel-release
 yum install -y net-tools git autoconf automake libtool gcc-c++ libuuid-devel zlib-devel libjpeg-devel ncurses-devel openssl-devel sqlite-devel curl-devel pcre-devel speex-devel ldns ldns-devel libedit-devel gtk+-devel gtk2-devel yasm-devel lua-devel opus-devel e2fsprogs-devel libyuv-devel lua-devel libsndfile-devel libshout-devel lame-devel libvpx-devel opusfile libbroadvoice-dev libtiff-*
